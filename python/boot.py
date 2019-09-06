6bdd82eaa631bf4b75a4025608088bc74c7fd1dc
Oliver Robson <oliver.robson.ecse@gmail.com>
2019-09-06 13:58:10 +1000

    Fixing linting issues in `boot.py`

--- python/boot.py
+++ python/boot.py
@@ -1,19 +1,22 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 
 # Stuff that came with MicroPython
-#import esp
-#esp.osdebug(None)
-import uos, machine
-#uos.dupterm(None, 1) # disable REPL on UART(0)
+# import esp
+# esp.osdebug(None)
+# import uos
+import machine
+# uos.dupterm(None, 1) # disable REPL on UART(0)
 import gc
-#import webrepl
-#webrepl.start()
+# For RGB LED control
+import wemos
+import neopixel
+# import webrepl
+# webrepl.start()
 gc.collect()
 
 # Default to RGB LED shield being disabled (if connected)
-import wemos, neopixel, machine
 np = neopixel.NeoPixel(machine.Pin(wemos.D4), 7)
-np.fill((0,0,0))
+np.fill((0, 0, 0))
 np.write()
 del(np)
 gc.collect()
