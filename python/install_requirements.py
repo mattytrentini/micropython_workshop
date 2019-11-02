b814aa91f0c80964b6fb58471cef10433e033e36
Oliver Robson <oliver.robson.ecse@gmail.com>
2019-11-02 18:48:13 +1100

    Change `uasyncio` install method to copy (from `upip`)
    * Removes WiFi dependency

--- python/install_requirements.py
+++ python/install_requirements.py
@@ -1,26 +1,13 @@
 import pyboard
 import sys
 
-COMMAND, PORT = sys.argv[1:3]
+PORT = sys.argv[1]
 
 pyb = pyboard.Pyboard(PORT)
 pyb.enter_raw_repl()
 
 result = 0
-
-if COMMAND == "INSTALL":
-    SSID, PWRD = sys.argv[3:5]
-    out = pyb.exec("""
-import network, upip
-sta_if = network.WLAN(network.STA_IF)
-sta_if.active(True)
-sta_if.connect('%s', '%s')
-while not sta_if.isconnected(): pass
-upip.install('uasyncio')
-""" % (SSID, PWRD))
-    print(out)
-elif COMMAND == "TEST":
-    out = pyb.exec("""
+out = pyb.exec("""
 try:
     import d1_mini
     import uasyncio
@@ -32,9 +19,9 @@ except Exception as e:
 else:
     print('Test successful!')
     """)
-    print(out)
-    if b'Test successful!' not in out:
-        result = -1
+print(out)
+if b'Test successful!' not in out:
+    result = -1
 
 pyb.exit_raw_repl()
 result
